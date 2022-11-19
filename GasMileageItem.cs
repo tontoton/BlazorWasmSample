447f8c7f8092641521471b50cdda01c085bf9f5a
arisaka <arisakagm@gmail.com>
2022-11-19 09:42:49 +0900

    デザインを仮で設定

--- GasMileageItem.cs
+++ GasMileageItem.cs
@@ -8,20 +8,41 @@ public class GasMileageItem
     /// </summary>
     [Required]
     public double Odometer { get; set; }
+
+    public string GetOdometerString()
+    {
+        // return $"{Odometer:#,0:F1}";
+        return $"{Odometer:n1} km";
+    }
     
     /// <summary>
     /// 走行距離
     /// </summary>
     public double Mileage { get; set; }
+
+    public string GetMileageString()
+    {
+        return $"{Mileage:n1} km";
+    }
     
     /// <summary>
     /// 給油量
     /// </summary>
     [Required]
     public double OilingQuantity { get; set; }
+
+    public string GetOilingQuantityString()
+    {
+        return $"{OilingQuantity:n2} L";
+    }
     
     /// <summary>
     /// 燃費
     /// </summary>
     public double GasMileage { get; set; }
+
+    public string getGasMileageString()
+    {
+        return $"{GasMileage:n2} km/L";
+    }
 }
